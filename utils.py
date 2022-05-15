Fixed bugs in test(), add precision

utils.py
@@ -165,7 +165,7 @@ def measure_012(expected, predicted):
     INC = torch.sum((expected!=predicted).long()).cpu().item()
     return TP, FP, TN, FN, ACC, INC
 
-def measure_to_str(TP, FP, TN, FN, ACC, threshold):
-    return 'TP%d: %0.3f, FP%d: %0.3f, TN%d: %0.3f,FN%d: %0.3f,ACC%d: %0.3f' % (threshold,TP,threshold,FP,threshold,TN,threshold,FN,threshold,ACC)
+def measure_to_str(TP, FP, TN, FN, ACC, threshold,PRECISION):
+    return 'TP%d: %0.3f, FP%d: %0.3f, TN%d: %0.3f,FN%d: %0.3f,ACC%d: %0.3f,PRECISION%d: %0.3f' % (threshold,TP,threshold,FP,threshold,TN,threshold,FN,threshold,ACC,PRECISION)
 
 