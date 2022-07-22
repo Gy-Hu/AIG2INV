4b2c1e408506a999280112e5a01a44de88151c77
GaryHU <swe1609011@gmail.com>
2022-07-22 17:05:11 +0800

    fix trivial bugs when import package, adjust file strucutre

--- train/train_first_clause.py
+++ train/train_first_clause.py
@@ -3,7 +3,6 @@ from logging import exception
 
 from zmq import FD
 import sys
-sys.path.append("../NN_models")
 sys.path.append("../data2dataset/aig2graph")
 from models_1stlayer_label import DGDAGRNN
 from utils import expand_clause_012, expand_clause, sum_clause, load_module_state,  measure_012, measure_to_str, set_label_weight, get_label_freq
