4b924b31750100d80a3be5d20d5768e64eaa6f98
GaryHU <swe1609011@gmail.com>
2023-03-12 21:56:03 +0800

    Tremendously improve the expr simplification speed

--- data2dataset/cex2smt2/sp_converter.py
+++ data2dataset/cex2smt2/sp_converter.py
@@ -8,6 +8,7 @@ from sympy.logic.boolalg import true, false
 import z3
 from functools import reduce
 import pycparser.c_ast as ast
+from functools import lru_cache
 
 # z3 <-> sympy
 
@@ -74,135 +75,56 @@ def z3_deep_simplify(expr):
     else:
         return z3.And(*[z3_deep_simplify(cond) for cond in cond_list])
 
+@lru_cache(maxsize=None)
 def to_z3(sp_expr):
-    self = sp.factor(sp_expr)
-    if isinstance(self, sp.Add):
-        res = sum([to_z3(arg) for arg in self.args])
-    elif isinstance(self, sp.Mul):
-        res = 1
-        for arg in reversed(self.args):
-            if arg.is_number and not arg.is_Integer:
-                res = (res*arg.numerator)/arg.denominator
-            else:
-                res = res * to_z3(arg)
-        return z3.simplify(res)
-        # return reduce(lambda x, y: x*y, [to_z3(arg) for arg in reversed(self.args)])
-    elif isinstance(self, sp.Piecewise):
-        if len(self.args) == 1:
-            res = to_z3(self.args[0][0])
-        else:
-            cond  = to_z3(self.args[0][1])
-            res = z3.If(cond, to_z3(self.args[0][0]), to_z3(self.args[1][0]))
-    elif isinstance(self, sp.And):
-        res = z3.And(*[to_z3(arg) for arg in self.args])
-    elif isinstance(self, sp.Or):
-        res = z3.Or(*[to_z3(arg) for arg in self.args])
-    elif isinstance(self, sp.Not):
-        res = z3.Not(*[to_z3(arg) for arg in self.args])
-    elif isinstance(self, sp.Gt):
-        res = to_z3(self.lhs) > to_z3(self.rhs)
-    elif isinstance(self, sp.Ge):
-        res = to_z3(self.lhs) >= to_z3(self.rhs)
-    elif isinstance(self, sp.Lt):
-        res = to_z3(self.lhs) < to_z3(self.rhs)
-    elif isinstance(self, sp.Le):
-        res = to_z3(self.lhs) <= to_z3(self.rhs)
-    elif isinstance(self, sp.Eq):
-        res = to_z3(self.lhs) == to_z3(self.rhs)
-    elif isinstance(self, sp.Ne):
-        res = to_z3(self.lhs) != to_z3(self.rhs)
-    elif isinstance(self, sp.Integer) or isinstance(self, int):
-        res = z3.IntVal(int(self))
-    elif isinstance(self, sp.Symbol):
-        #XXX: Double check before running script
-        # we assume that all symbols are boolean
+    self = sp.simplify(sp_expr)
+    
+    #self = sp.factor(sp_expr)
+    if isinstance(self, sp.Symbol):
         res = z3.Bool(str(self))
-        #res = z3.Int(str(self))
-    elif isinstance(self, sp.Rational):
-        # return z3.RatVal(self.numerator, self.denominator)
-        res = z3.IntVal(self.numerator) / z3.IntVal(self.denominator)
-    elif isinstance(self, sp.Pow):
-        if self.base == 0: res = z3.IntVal(0)
-        else: raise Exception('%s' % self)
-    elif isinstance(self, sp.Mod):
-        res = to_z3(self.args[0]) % to_z3(self.args[1])
-    elif isinstance(self, sp.Abs):
-        res = z3.Abs(to_z3(self.args[0]))
-    elif isinstance(self, sp.Sum):
-        s = z3.Function('Sum', z3.IntSort(), z3.IntSort(), z3.IntSort(), z3.IntSort(), z3.IntSort())
-        # expr, (idx, start, end) = self.args
-        expr, *l = self.args
-        res = to_z3(expr)
-        for idx, start, end in l:
-            res = s(res, to_z3(idx), to_z3(start), to_z3(end))
     elif self is true:
         res = z3.BoolVal(True)
     elif self is false:
         res = z3.BoolVal(False)
-    elif self.is_Function:
-        func = self.func
-        args = self.args
-        z3_func = z3.Function(func.name, *([z3.IntSort()]*(len(args) + 1)))
-        res = z3_func(*[to_z3(arg) for arg in args])
+    elif isinstance(self, sp.Not):
+        res = z3.Not(to_z3(self.args[0]))
+    elif isinstance(self, (sp.And, sp.Or)):
+        args = [to_z3(arg) for arg in self.args]
+        if isinstance(self, sp.And):
+            res = z3.And(*args)
+        else:
+            res = z3.Or(*args)
     else:
         raise Exception('Conversion for "%s" has not been implemented yet: %s' % (type(self), self))
-    return z3.simplify(res)
+    #return z3.simplify(res)
+    return res
 
+@lru_cache(maxsize=None)
 def to_sympy(expr):
-    if z3.is_int_value(expr):
-        res = expr.as_long()
-    elif z3.is_const(expr) and z3.is_bool(expr):
-        #XXX: Double check before running script
-        #res = sp.S.true if z3.is_true(expr) else sp.S.false
-        # initialize the boolean variable
-        res = sp.Symbol(str(expr))
-    elif z3.is_const(expr):
-        res = sp.Symbol(str(expr), integer=True)
-    elif z3.is_add(expr):
-        res = sum([to_sympy(arg) for arg in expr.children()])
-    elif z3.is_sub(expr):
-        children = expr.children()
-        assert(len(children) == 2)
-        res = to_sympy(children[0]) - to_sympy(children[1])
-    elif z3.is_mul(expr):
-        children = expr.children()
-        res = reduce(lambda x, y: x*y, [to_sympy(ch) for ch in children])
-    elif z3.is_mod(expr):
-        children = expr.children()
-        res = to_sympy(children[0]) % to_sympy(children[1])
-    elif z3.is_gt(expr):
-        children = expr.children()
-        res = to_sympy(children[0]) > to_sympy(children[1])
-    elif z3.is_lt(expr):
-        children = expr.children()
-        res = to_sympy(children[0]) < to_sympy(children[1])
-    elif z3.is_ge(expr):
-        children = expr.children()
-        res = to_sympy(children[0]) >= to_sympy(children[1])
-    elif z3.is_le(expr):
-        children = expr.children()
-        res = to_sympy(children[0]) <= to_sympy(children[1])
-    elif z3.is_eq(expr):
-        children = expr.children()
-        res = sp.Eq(to_sympy(children[0]), to_sympy(children[1]))
-    elif z3.is_not(expr):
-        children = expr.children()
-        res = sp.Not(to_sympy(children[0]))
-    elif z3.is_and(expr):
-        children = expr.children()
-        body = [to_sympy(ch) for ch in children]
-        res = sp.And(*body)
-    elif z3.is_or(expr):
-        children = expr.children()
-        res = sp.Or(*[to_sympy(ch) for ch in children])
-    elif len(expr.children()) == 3 and z3.is_bool(expr.children()[0]):
-        children = expr.children()
-        cond = to_sympy(children[0])
-        res = sp.Piecewise((to_sympy(children[1]), cond), (to_sympy(children[2]), sp.S.true))
+    # check whether the expression is a sympy expression
+    if isinstance(expr, sp.Expr):
+        return expr
+    
+    if z3.is_const(expr):
+        if z3.is_bool(expr):
+            res = sp.Symbol(str(expr))
+        else:
+            res = sp.Symbol(str(expr), integer=True)
+    elif z3.is_app_of(expr, z3.Z3_OP_NOT):
+        res = sp.Not(to_sympy(expr.arg(0)))
+    elif z3.is_app_of(expr, z3.Z3_OP_AND) or z3.is_app_of(expr, z3.Z3_OP_OR):
+        op = expr.decl().name()
+        children = [to_sympy(child) for child in expr.children()]
+        if op == 'and':
+            res = sp.And(*children)
+        elif op == 'or':
+            res = sp.Or(*children)
+        else:
+            raise Exception('Conversion for "%s" has not been implemented yet: %s' % (op, expr))
     else:
         raise Exception('conversion for type "%s" is not implemented: %s' % (type(expr), expr))
-    return sp.simplify(res)
-    #return res
+    #return sp.simplify(res)
+    return res
 
 def get_app_by_var(var, expr):
     '''for sympy'''
@@ -374,7 +296,7 @@ if __name__ == '__main__':
     a = z3.Bool('a')
     b = z3.Bool('b')
     c = z3.Not(z3.And(z3.Not(a), z3.Not(b)))
-    print(to_sympy(c))
+    print(z3.simplify(to_z3(to_sympy(c))))
     
     # a = sp.Function('a')
     # bb = sp.Function('bb')
