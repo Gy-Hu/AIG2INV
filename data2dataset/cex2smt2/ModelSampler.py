ad1b94f0385f14fc0e5c9bd7ae9722eb549e1640
GaryHU <swe1609011@gmail.com>
2023-03-15 00:43:10 +0800

    Enhance utils that generate random SAT model in uniform distribution

--- data2dataset/cex2smt2/ModelSampler.py
+++ data2dataset/cex2smt2/ModelSampler.py
@@ -1,16 +1,14 @@
-'''
-Randomize the model from a given z3 context
-'''
-
-from z3 import *          
+from z3 import *
+import time
 import random
-                                                                                                                                                                                     
+
 class z3_workaround:
-    def __init__(self, decls, assertions, seed):
+    def __init__(self, decls, assertions, seed, old_solver=None):
         self.assertions = assertions
         self.decls = decls
-        self.solver_ctx = Context()
-        self.solver = Solver(ctx=self.solver_ctx)
+        #self.solver_ctx = Context()
+        #self.solver = Solver(ctx=self.solver_ctx)
+        self.solver = old_solver
         self.solver.set("random_seed", seed)
         self.solver.set("phase_selection", 5)
         print("seed is ", seed)
@@ -20,7 +18,7 @@ class z3_workaround:
         decl_vars.sort()
         for x in decl_vars:
             #print(f'{x} = BitVec(\'{x}\',32)')
-            exec(f'{x} = Bool(\'{x}\', self.solver_ctx)')
+            exec(f'{x} = Bool(\'{x}\')')
 
         asts = [str(x) for x in self.assertions] 
         asts.sort() 
@@ -34,17 +32,35 @@ class z3_workaround:
     def model(self):
         return self.solver.model()
     
+def all_smt(s, initial_terms):
+    def block_term(s, m, t):
+        s.add(t != m.eval(t))
+    def fix_term(s, m, t):
+        s.add(t == m.eval(t))
+    def all_smt_rec(terms):
+        if sat == s.check():
+           seed = random.randint(0, 2 ** 30)
+           _solution = s.model()
+           zw = z3_workaround(_solution.decls(), s.assertions(), seed,s)
+           zw.check()
+           m = zw.model()
+           #m = s.model()
+           yield m
+           for i in range(len(terms)):
+               s.push()
+               block_term(s, m, terms[i])
+               for j in range(i):
+                   fix_term(s, m, terms[j])
+               yield from all_smt_rec(terms[i:])
+               s.pop()   
+    yield from all_smt_rec(list(initial_terms))  
+
 if __name__ == "__main__":
-    # generate a random seed
-    #seed = random.randint(0, 2 ** 30)
-    seed = rand_num = random.SystemRandom().randint(0, 100)
+    start_time = time.time()
     a, b, c = Bools('a b c')
-    prev_solver = Solver()
-    #prev_solver.set("model.completion", True)
-    prev_solver.add(Or(a, b, c))
-    prev_solver.check()
-    _solution = prev_solver.model()
-    print(_solution.decls())
-    zw= z3_workaround(_solution.decls(), prev_solver.assertions(), seed)
-    zw.check()
-    print(zw.model())
+    s = Solver()
+    s.add(Or(a, b, c))
+    print(s)
+    models = list(all_smt(s,[a,b,c]))
+    print(models)
+    print(time.time()-start_time)
