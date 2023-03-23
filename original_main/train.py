653ac5055782065f784f71bb1e01dc282b0746f2
GaryHU <swe1609011@gmail.com>
2023-03-23 11:34:48 +0800

    remove unused files

--- original_main/train.py
+++
@@ -1,6 +0,0 @@
-import train_circuitsat as train_with_circuitsat
-import train_neuograph as train_with_neurograph
-
-if __name__ == '__main__':
-    train_with_neurograph.run_train_with_neuro_graph()
-    #train_with_circuitsat.run_train_with_circuit_sat()
