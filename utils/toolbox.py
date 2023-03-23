4fb8116315f48649da5ba3aa14efff7a877f5773
GaryHU <swe1609011@gmail.com>
2023-03-23 11:39:38 +0800

    remove unused files

--- utils/toolbox.py
+++
@@ -1,8 +0,0 @@
-import os
-from natsort import natsorted
-
-def walkFile(dir):
-    for root, _, files in os.walk(dir):
-        files = natsorted(files)
-        files = [os.path.join(root,f) for f in files]
-    return files
